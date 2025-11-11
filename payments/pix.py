b4d37092138e3fffd10d3111fd77e37704b21873
Yago Felix <yagofelix007@gmail.com>
2025-11-10 23:53:23 -0300

    feat: add basic Pix payment generator (UUID, hash and QR code)

---
+++ payments/pix.py
@@ -0,0 +1,21 @@
+import uuid
+import qrcode
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # criar o pagamento na instituicao financeira
+        bank_payment_id = uuid.uuid4()
+
+        # codigo_copia_e_cola_123
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        # qr code
+        img = qrcode.make(hash_payment)
+
+        # salvar a imagem como arquivo PNG
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {"bank_payment_id": bank_payment_id,
+                "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
