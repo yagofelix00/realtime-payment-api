f20bae0a173c900cda7e44262c75ac1c0794cc6d
Yago Felix <yagofelix007@gmail.com>
2025-11-17 23:59:49 -0300

    feat(api): implement PIX payment confirmation route >> >> >> >> - Added POST /payments/pix/confirmation endpoint >> >> - Added payload validation for bank_payment_id >> >> - Added database lookup for corresponding Payment >> >> - Returned proper success and error responses

--- app.py
+++ app.py
@@ -42,7 +42,19 @@ def get_image(file_name):
 
 
 @app.route('/payments/pix/confirmation', methods=["POST"])
+def pix_confirmation():
+    data = request.get_json()
 
+    # validations
+    if "bank_payment_id" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+    
+    # payment
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment:
+        jsonify({"message": "Payment not found"}), 404
+    return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
