e39e275c6734ae9b01cd1dfe59d34749b54370b9
Yago Felix <yagofelix007@gmail.com>
2025-12-12 02:31:50 -0300

    feat: improve payment route handling and update 404 page template
    Updated the /payments/pix/<payment_id> route to ensure consistent rendering logic and improved response handling for missing payments.
    Refactored the 404.html template for cleaner structure and consistency with the updated backend flow.

--- app.py
+++ app.py
@@ -65,6 +65,10 @@ def pix_confirmation():
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if not payment:
+        return render_template("404.html")
+    
     
     if payment.paid:
         return render_template('confirmed_payment.html',
