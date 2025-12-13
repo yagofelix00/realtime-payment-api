dabb734c9625d85f21a1140a42473fce23a712de
Yago Felix <yagofelix007@gmail.com>
2025-12-13 13:49:45 -0300

    test: add integration test for Pix QR code generation

--- payments/pix.py
+++ payments/pix.py
@@ -4,7 +4,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar o pagamento na instituicao financeira
         bank_payment_id = str(uuid.uuid4())
 
@@ -15,7 +15,7 @@ class Pix:
         img = qrcode.make(hash_payment)
 
         # salvar a imagem como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {"bank_payment_id": bank_payment_id,
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
